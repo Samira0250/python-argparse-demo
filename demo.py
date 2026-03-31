f8121e94ccdad0fa2fecb8bc07153843f7b9e933
Samira Salimiyan <szs0394@cvmpatmb7915.lan>
2026-03-31 15:00:47 -0500

    updating

--- demo.py
+++ demo.py
@@ -1,73 +1,56 @@
-#! /usr/bin/env python3
-
-
-"A demostration of using the argparse module to process command-line arguments."
+#!/usr/bin/env python3
+"""
+A word search demo using argparse.
+This script reads one or more text files, searches for words containing a specified pattern,
+prints matches for each file, and saves all matches to 'matches.txt'.
+"""
 
 import argparse
+import re
 
 if __name__ == '__main__':
-    # Create a command-line parser object
-    # We will specify a `formatter_class` that includes the default values for
-    # arguments in the help documentation.
+    # Create parser
     parser = argparse.ArgumentParser(
-        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+        description="Search for words containing a pattern in one or more text files.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    # This `add_argument` method specifies that this script expects 1 or more
-    # file paths as positional arguments
+    # Positional argument: one or more file paths
     parser.add_argument(
-        'file_path',                # The name we'll use to access this argument
-        type = str,                  # The type of the argument
-        metavar = 'FILE-PATH',       # The placeholder name in the help menu
-        nargs = '+',                 # We expect 1 or more file paths
-        help = 'A path to a file.',  # The help menu text
+        'file_path',
+        type=str,
+        metavar='FILE-PATH',
+        nargs='+',
+        help='Path(s) to text file(s) to search.',
     )
 
-    # Using the `add_argument` method again to create an optional keyword
-    # argument for an integer
+    # Optional argument: word/pattern to search for
     parser.add_argument(
-        '-n', '--number',     # The flags used to specify this option
-        type = int,
-        default = 1,          # Specifying a default value
-        help = 'An integer.',
+        '-w', '--word',
+        type=str,
+        default='herit',
+        help='Word or pattern to search for in files.',
     )
 
-    # Adding another optional keyword argument for a floating-point number
-    parser.add_argument(
-        '-t', '--threshold',
-        type = float,
-        default = 3.4,
-        help = 'A super duper important threshold.',
-    )
-
-    # Adding another optional boolean keyword argument
-    parser.add_argument(
-        '-c', '--i-am-cool',
-        default = False,
-        action = 'store_true',
-        help = 'A boolean option.',
-    )
-
-    # Parse the command-line arguments into a 'dict'-like container
+    # Parse arguments
     args = parser.parse_args()
 
-    # What does `args` object "look" like?
-    print(
-        "The args after being processed by the argparse parser object:\n",
-        args)
+    # Compile regex pattern for the search word (case-insensitive)
+    pattern = re.compile(rf'\w*{args.word}\w*', re.IGNORECASE)
 
-    # We access the arguments specified on the command line (or the default
-    # values if they were not used) by using `args.` syntax.
-    #
-    # For positional arguments (like the first `add_argument` above), we access
-    # the value of the arugment using the name we provided as the first
-    # argument ("paths")
-    print("Paths:", args.file_path)
+    all_matches = []
 
-    # For keyword arguments (like all the rest of the `add_argument` calls
-    # above), we access the value of the argument using the the long form of
-    # the keyword flag (removing the first two dashes and replacing any other
-    # dashes with underscores)
-    print("Number:", args.number)
-    print("Threshold:", args.threshold)
-    print("I am cool?", args.i_am_cool)
+    # Process each file
+    for file in args.file_path:
+        try:
+            with open(file, 'r') as f:
+                text = f.read()
+            matches = pattern.findall(text)
+            print(f"Matches in {file}: {matches}")
+            all_matches.extend(matches)
+        except FileNotFoundError:
+            print(f"File '{file}' not found!")
+
+    # Save all matches to a file
+    with open("matches.txt", "w") as f:
+        f.write("\n".join(all_matches))
